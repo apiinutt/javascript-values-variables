609a7144b643c16eef3dc63f524667f6fdab93de
apiinutt <apinut19501@gmail.com>
2025-12-01 21:12:03 +0700

    submit assignment

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,17 @@
 //Exercise #1: Variable Creation
 //Start coding here
-
+let firstName = 'Karter';
+let surName = 'Anderson';
+let address = '54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai';
+const restaurantName = "Chef's Table by Chef James"; 
+const totalPrice = 15000 ;
+const totalRestaurantOpening = 1500 ;
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = '(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641';
+totalPrice = 5000;
+totalRestaurantOpening = 2500;
+
+console.log (address);
+console.log (totalPrice);
+console.log (totalRestaurantOpening);
